d2432079c5303d098b333715174cf0b760fd1bb1
dgl1231 <dgl12312@naver.com>
2023-04-12 14:07:00 +0900

    WhyUseDi

---
+++ Services/GetHelloWorldQuery.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class GetHelloWorldQuery
+    {
+        public GetHelloWorldQuery()
+        {
+
+        }
+
+        public string Get()
+        {
+            return "HelloWorld";
+        }
+    }
+}
---
+++ WhyUseDi/Controllers/HelloWorldController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WhyUseDI.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloWorldController : ControllerBase
+    {
+        private readonly ILogger<HelloWorldController> _logger;
+        private readonly GetHelloWorldQuery _service;
+
+        public HelloWorldController(ILogger<HelloWorldController> logger, GetHelloWorldQuery service)
+        {
+            _logger = logger;
+            _service = service;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return _service.Get();
+        }
+    }
+}

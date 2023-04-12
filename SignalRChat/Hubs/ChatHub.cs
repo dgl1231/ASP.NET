2ddc3696a8f43675833523f1d109aab95ce2f7c1
dgl1231 <dgl12312@naver.com>
2023-04-12 09:20:36 +0900

    chat system

--- SignalRChat/Hubs/ChatHub.cs
+++ SignalRChat/Hubs/ChatHub.cs
@@ -1,13 +1,47 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using System;
 using System.Threading.Tasks;
 
 namespace SignalRChat.Hubs
 {
-    public class ChatHub : Hub
+    public interface IChatClient
+    {
+        Task ReceiveMessage(string user, string message);
+    }
+    public class ChatHub : Hub<IChatClient>
     {
         public async Task SendMessage(string user, string message)
         {
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
+            //await Clients.All.SendAsync("ReceiveMessage", user, message);
+            await Clients.All.ReceiveMessage(user, message);
+        }
+        public Task SendMessageToCaller(string user, string message)
+        {
+            //await Clients.Caller.SendAsync("ReceiveMessage", user, message);
+            return Clients.Caller.ReceiveMessage(user, message);
+
+        }
+        public async Task SendMessageToGroup(string user, string message)
+        {
+            //await Clients.Group("SignalR Users").SendAsync("ReceiveMessage", user, message);
+            await Clients.Group("SignalR Users").ReceiveMessage(user, message);
+
+        }
+        [HubMethodName("SendMessageToUser")]
+        public Task DirectMessage(string user, string message)
+        {
+            return Clients.User(user).ReceiveMessage(user, message);
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, "SignalR Users");
+            await base.OnConnectedAsync();
+        }
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            await Clients.Group("SignalR Users").ReceiveMessage("I", "disconnect");
+            await base.OnDisconnectedAsync(exception);
         }
     }
 }
